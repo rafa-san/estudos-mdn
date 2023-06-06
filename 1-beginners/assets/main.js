4239099b581447e415e13d8aaa757ab45257263d
Rafael <88090582+rafa-san@users.noreply.github.com>
2023-06-06 16:33:47 -0300

    Atualizações:
    Adicionei exemplos e exercícios de HTML, CSS e JavaScript.

--- 1-beginners/assets/main.js
+++ 1-beginners/assets/main.js
@@ -10,28 +10,56 @@ myImage.onclick = () => {
   }
 };
 
-// INCLUIR NOME
+// // INCLUIR NOME
+// let myButton = document.querySelector('button');
+// let myHeading = document.querySelector('h1')
+
+// function setUserName() {
+//   const myName = prompt('Por favor, digite seu nome.');
+//   if (!myName) {
+//     setUserName();
+//   } else {
+//     localStorage.setItem('name', myName);
+//     myHeading.textContent = `Olá, ${myName}! Essa é a Minha Futura Bicicleta.`;
+//   }
+  
+// }
+
+// if (!localStorage.getItem('name')) {
+//   setUserName();
+// }   else {
+//     const storedName = localStorage.getItem('name');
+//     myHeading.textContent = `Olá, ${storedName}! Essa é a Minha Futura Bicicleta.`;
+//   }
+
+// myButton.onclick = () => {
+//   setUserName();
+// };
+
+
+
+// Personalized welcome message code
+
 let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1')
+let myHeading = document.querySelector('h1');
 
 function setUserName() {
-  const myName = prompt('Por favor, digite seu nome.');
-  if (!myName) {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
     setUserName();
   } else {
     localStorage.setItem('name', myName);
-    myHeading.textContent = `Olá, ${myName}! Essa é a Minha Futura Bicicleta.`;
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
   }
-  
 }
 
-if (!localStorage.getItem('name')) {
+if(!localStorage.getItem('name')) {
   setUserName();
-}   else {
-    const storedName = localStorage.getItem('name');
-    myHeading.textContent = `Olá, ${storedName}! Essa é a Minha Futura Bicicleta.`;
-  }
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
 
-myButton.onclick = () => {
+myButton.onclick = function() {
   setUserName();
-};
+}
