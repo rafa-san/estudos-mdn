85086d90dac50c88f3b86d64b1135f1def42de21
Rafael <88090582+rafa-san@users.noreply.github.com>
2024-04-19 10:36:31 -0300

    Criação dos scripts dos exemplos

---
+++ scripts/04-javascript/01-basics/exemplo-01.js
@@ -0,0 +1,6 @@
+const mensagem = document.querySelector('.retorno'); 
+mensagem.textContent = 'Olá, mundo!'; 
+
+/*
+Acima definimos o valor da propriedade textContent para "Olá, mundo!". Poderíamos usar também o innerHTML no lugar do textContent.
+*/
