4e439334deba7d4b8ffc8f6d77c6b0d9611dc949
Rafael <88090582+rafa-san@users.noreply.github.com>
2024-04-20 10:34:10 -0300

    Atualizei trechos do script

--- scripts/beginners/my-test-page.js
+++ scripts/beginners/my-test-page.js
@@ -32,24 +32,16 @@ let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
 function setUserName() {
-  let myName = prompt('Please enter your name.');
-  if(!myName) {
-    setUserName();
-  } else {
-    localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
-  }
+  const myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Mozilla is cool, ${myName}`;
 }
 
-if(!localStorage.getItem('name')) {
+if (!localStorage.getItem("name")) {
   setUserName();
 } else {
-  let storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
-}
-
-myButton.onclick = function() {
-  setUserName();
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
 }
 
 /*
