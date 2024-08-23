94880c66b5df2d02a017d410a6f25059dd57eac6
Rafael <88090582+rafa-san@users.noreply.github.com>
2024-08-23 16:16:50 -0300

    Criação de novos exemplos e exercícios

---
+++ 03-javascript/02-first-steps/05-basic-math-in-javascript/exemplos.js
@@ -0,0 +1,32 @@
+/*
+-> Exemplo 1
+const myInt = 5;
+const myFloat = 6.667;
+console.log(myInt);
+console.log(typeof myInt);
+console.log(myFloat);
+
+
+-> Exemplo 2
+const lotsOfDecimal = 1.766584958675746364;
+console.log(lotsOfDecimal);
+const twoDecimalPlaces = lotsOfDecimal.toFixed(2);
+console.log(twoDecimalPlaces);
+
+
+-> Exemplo 3
+let myNumber = "74";
+//myNumber += 3;
+console.log(typeof myNumber);
+myNumber = Number(myNumber) + 3;
+console.log(myNumber);
+
+
+-> Exemplo 4
+let num1 = 4;
+console.log(num1++); aqui o número é incrementado...
+console.log(num1); // ...mas é aqui que aparece o número com o valor atualizado
+
+console.log(++num1); // E aqui ele já mostra o número "incrementado"
+console.log(num1);
+*/
