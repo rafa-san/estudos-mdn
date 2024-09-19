b4564aa90060e1ee25fad8690983ee8a97114b06
Rafael <88090582+rafa-san@users.noreply.github.com>
2024-09-19 10:40:36 -0300

    Criação de novos arquivos

---
+++ 03-javascript/02-first-steps/07-useful-string-methods/rascunhos.js
@@ -0,0 +1,62 @@
+
+
+
+
+console.log(finalQuote);
+
+
+
+
+
+
+
+
+
+
+/*
+
+let teste = quote.toLowerCase() + quote.at(0).toUpperCase();
+console.log(teste);
+
+
+// Corrigir a frase para "sentence case" (primeira letra maiúscula, restante minúsculo)
+let fixedQuote = quote.toLowerCase();
+fixedQuote = fixedQuote.at(0).toUpperCase() + fixedQuote.slice(1);
+
+// Substituir "green eggs and ham" por outro alimento
+fixedQuote = fixedQuote.replace('green eggs and ham', 'broccoli and spinach');
+
+// Adicionar o ponto final
+if (fixedQuote.endsWith('.')) {
+  console.log(fixedQuote)
+} else {
+  console.log(`${fixedQuote}.`)
+}
+
+console.log(fixedQuote);
+
+
+*/
+
+
+
+//const finalQuote = fixedQuote.endsWith('.') ? fixedQuote : fixedQuote + '.';
+
+
+//const fixedQuote = `${quote[0]} ${quote.toLowerCase().slice(2).replace("green eggs and ham", "chicken and coca-cola")}.`;
+
+//console.log(fixedQuote);
+
+
+/*
+const fixedQuote = `${quote.toLowerCase().replace("i", "I").replace("green eggs and ham", "chicken and coca-cola")}.`;
+
+console.log(fixedQuote);
+
+
+`${quote[0]}`
+
+
+
+quote[0] + quote.slice(1).toLowerCase();
+*/
---
+++ 03-javascript/02-first-steps/08-arrays/rascunhos.js
@@ -0,0 +1,19 @@
+/*
+const sequence = [1, 1, 2, 3, 5, 8, 13];
+console.log(sequence);
+
+
+const random = ["tree", 795, [0, 1, 2]];
+console.log(random);
+console.log(random[2][2]);
+
+*/
+
+
+
+const shopping = ["bread", "milk", "cheese", "hummus", "noodles"];
+//console.log(shopping); // [ 'bread', 'milk', 'cheese', 'hummus', 'noodles' ]
+//console.log(shopping.length); // 5
+console.log(shopping[1]); // milk
+shopping[1] = "tahini";
+console.log(shopping); // [ 'bread', 'tahini', 'cheese', 'hummus', 'noodles' ]
